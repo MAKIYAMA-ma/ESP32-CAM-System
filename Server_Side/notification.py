d37337d6d1783efb631e141a7861237ebcd99e0b
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-06-27 06:21:03 +0900

    add base script to send email

---
+++ Server_Side/notification.py
@@ -0,0 +1,39 @@
+from email.message import EmailMessage
+import email_setting
+import smtplib
+import sys
+import ssl
+
+
+def create_message_body(title: str, body: str):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg["Subject"] = title
+    msg["From"] = email_setting.from_address
+    msg["To"] = email_setting.to_address
+
+    return msg
+
+
+def send_mail(msg: EmailMessage):
+    # context = ssl.create_default_context()
+    # server = smtplib.SMTP_SSL(email_setting.smtp_host, email_setting.smtp_port, context=context)
+    server = smtplib.SMTP(email_setting.smtp_host, email_setting.smtp_port)
+    server.login(email_setting.username, email_setting.password)
+    server.send_message(msg)
+    server.quit()
+
+
+def main(title: str, body: str):
+    send_mail(create_message_body(title, body))
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python notification.py <title> <message>")
+        sys.exit(1)
+
+    title = sys.argv[1]
+    message = sys.argv[2]
+
+    main(title, message)
