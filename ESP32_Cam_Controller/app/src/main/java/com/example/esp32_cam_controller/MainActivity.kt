2acd0806717f97b4c73322bcc53c1ed7c29599a1
DESKTOP-R4JOG3F\aaayw <aaaywr29@yahoo.co.jp>
2024-07-15 08:56:18 +0900

    - remove unnecessry files - imlement to send "shot" message with SHOT button

--- ESP32_Cam_Controller/app/src/main/java/com/example/esp32_cam_controller/MainActivity.kt
+++ ESP32_Cam_Controller/app/src/main/java/com/example/esp32_cam_controller/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.esp32_cam_controller
 
 import android.os.Bundle
+import android.content.Context
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,7 +22,6 @@ import android.content.IntentFilter
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mqttClient: MqttAndroidClient
-    private lateinit var receiver: MyReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +34,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         val brokerUrl = "tcp://192.168.0.8:1883"
-        val clientId = "KotlinAndroidClient"
+        val clientId = "ESP32-CAM-Controller"
         mqttClient = MqttAndroidClient(this, brokerUrl, clientId)
-
-        val existingCallback = object : MqttCallback {
+        mqttClient.setCallback(object : MqttCallback {
             override fun connectionLost(cause: Throwable?) {
                 println("Connection lost: ${cause?.message}")
             }
@@ -46,57 +48,152 @@ class MainActivity : AppCompatActivity() {
             override fun deliveryComplete(token: IMqttDeliveryToken?) {
                 println("Delivery complete")
             }
-        }
+        })
 
-        // val customCallback = CustomMqttCallback(this, existingCallback)
-        // mqttClient.setCallback(customCallback)
-
-        // receiver = MyReceiver()
-        // val filter = IntentFilter("android.net.conn.CONNECTIVITY_CHANGE")
-        // registerReceiver(receiver, filter)
-
-        connectAndPublish()
+        // connectAndPublish()
+        connect(this)
+        val btShot = findViewById<Button>(R.id.shotButton)
+        val listener = ShotButtonListerner()
+        btShot.setOnClickListener(listener)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        unregisterReceiver(receiver)
     }
 
-    private fun connectAndPublish() {
-        println("connectAndPublish");
+    companion object {
+        const val TAG = "ESP32-CAM-Controller"
+    }
 
+    fun connect(context: Context) {
+        val options = MqttConnectOptions().apply {
+            isAutomaticReconnect = true
+            isCleanSession = true
+        }
         try {
-            val options = MqttConnectOptions().apply {
-                isAutomaticReconnect = true
-                isCleanSession = true
-            }
-
             mqttClient.connect(options, null, object : IMqttActionListener {
                 override fun onSuccess(asyncActionToken: IMqttToken?) {
-                    println("Connected to broker")
-
-                    val topic = "test/topic"
-                    val message = MqttMessage("Hello from Android Kotlin".toByteArray())
-                    message.qos = 2
-
-                    mqttClient.publish(topic, message, null, object : IMqttActionListener {
-                        override fun onSuccess(asyncActionToken: IMqttToken?) {
-                            println("Message published")
-                        }
-
-                        override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                            println("Failed to publish message: ${exception?.message}")
-                        }
-                    })
+                    Log.d(TAG, "Connection success")
                 }
 
                 override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
-                    println("Failed to connect to broker: ${exception?.message}")
+                    Log.d(TAG, "Connection failure")
                 }
             })
         } catch (e: MqttException) {
             e.printStackTrace()
+        }
+    }
+
+    fun subscribe(topic: String, qos: Int = 1) {
+        try {
+            mqttClient.subscribe(topic, qos, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "Subscribed to $topic")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Failed to subscribe $topic")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun unsubscribe(topic: String) {
+        try {
+            mqttClient.unsubscribe(topic, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "Unsubscribed to $topic")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Failed to unsubscribe $topic")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun publish(topic: String, msg: String, qos: Int = 1, retained: Boolean = false) {
+        try {
+            val message = MqttMessage()
+            message.payload = msg.toByteArray()
+            message.qos = qos
+            message.isRetained = retained
+            mqttClient.publish(topic, message, null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "$msg published to $topic")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Failed to publish $msg to $topic")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    fun disconnect() {
+        try {
+            mqttClient.disconnect(null, object : IMqttActionListener {
+                override fun onSuccess(asyncActionToken: IMqttToken?) {
+                    Log.d(TAG, "Disconnected")
+                }
+
+                override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                    Log.d(TAG, "Failed to disconnect")
+                }
+            })
+        } catch (e: MqttException) {
+            e.printStackTrace()
+        }
+    }
+
+    // private fun connectAndPublish() {
+    //     println("connectAndPublish");
+
+    //     try {
+    //         val options = MqttConnectOptions().apply {
+    //             isAutomaticReconnect = true
+    //             isCleanSession = true
+    //         }
+
+    //         mqttClient.connect(options, null, object : IMqttActionListener {
+    //             override fun onSuccess(asyncActionToken: IMqttToken?) {
+    //                 println("Connected to broker")
+
+    //                 val topic = "test/topic"
+    //                 val message = MqttMessage("Hello from Android Kotlin".toByteArray())
+    //                 message.qos = 2
+
+    //                 mqttClient.publish(topic, message, null, object : IMqttActionListener {
+    //                     override fun onSuccess(asyncActionToken: IMqttToken?) {
+    //                         println("Message published")
+    //                     }
+
+    //                     override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+    //                         println("Failed to publish message: ${exception?.message}")
+    //                     }
+    //                 })
+    //             }
+
+    //             override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+    //                 println("Failed to connect to broker: ${exception?.message}")
+    //             }
+    //         })
+    //     } catch (e: MqttException) {
+    //         e.printStackTrace()
+    //     }
+    // }
+
+    // Listener
+    private inner class ShotButtonListerner : View.OnClickListener {
+        override fun onClick(view: View) {
+            publish("esp32-cam/control", "{ \"shot\" : true }")
         }
     }
 }
